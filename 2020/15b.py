94f9712b2fb4761fcb326acffee5d329175c0dbd
Magnus Morton <magnus@morton.ai>
2021-10-20 17:00:43 +0100

    fixed 15b apparently

--- 2020/15b.py
+++ 2020/15b.py
@@ -4,18 +4,16 @@ numbers = [int(num) for num in sys.stdin.readline().split(',')]
 history = {}
 
 for i, num in enumerate(numbers):
-    history[num] = (i+1,)
+    history[num] = i+1
 
 
 turn = len(numbers)
 
+prev = num
 while turn != 30000000:
     turn += 1
     last = history[num]
-    if len(last) > 1:
-        num = last[1] - last[0]
-    else:
-        num = 0
+    num = 0
     if num in history:
         history[num] = (history[num][-1], turn)
     else:
