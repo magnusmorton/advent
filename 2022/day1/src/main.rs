c81dd237fab9be345b7ce819baf82709d78d4f2a
Magnus Morton <magnusmorton@gmail.com>
2022-12-13 23:18:27 +0000

    1a done

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -1,12 +1,35 @@
-fn a1() {
+fn a1() -> i32{
     let input = std::fs::read_to_string("input.txt").unwrap();
     let split = input.split("\r\n\r\n");
+    let mut max = 0;
     for s in split {
-        println!("fooo: {}", s);
+        let lines = s.lines();
+        let cals = lines.map(|l| l.parse::<i32>().unwrap());
         
-    }
+        let tot = cals.sum();
+        if tot > max {
+            max = tot;
+        }
+    };
+    return max;
+}
+
+fn a2() -> i32{
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let split = input.split("\r\n\r\n");
+    let mut max = 0;
+    for s in split {
+        let lines = s.lines();
+        let cals = lines.map(|l| l.parse::<i32>().unwrap());
+        
+        let tot = cals.sum();
+        if tot > max {
+            max = tot;
+        }
+    };
+    return max;
 }
 
 fn main() {
-    a1();
+    println!("{}", a1());
 }
