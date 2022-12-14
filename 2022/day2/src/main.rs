f1215387d5e31e02be43ca22891953c41e4ee8df
Magnus Morton <magnusmorton@gmail.com>
2022-12-14 22:49:53 +0000

    2 part 2

--- 2022/day2/src/main.rs
+++ 2022/day2/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Lines;
+
 fn score_choice(token: &str) -> i32 {
     match token {
         "X" => 1,
@@ -22,11 +24,24 @@ fn score_round(opp: &str, me: &str) -> i32 {
         _ => 0,
     }
 }
+fn get_choice(opp: &str, me: &str) -> &'static str {
+  
+    match (opp, me) {
+        ("A", "X") => "Z",
+        ("A", "Y") => "X",
+        ("A", "Z") => "Y",
+        ("B", "X") => "X",
+        ("B", "Y") => "Y",
+        ("B", "Z") => "Z",
+        ("C", "X") => "Y",
+        ("C", "Y") => "Z",
+        ("C", "Z") => "X",
+        _ => "A",
+    }
+}
 
-fn main() {
-    let input = std::fs::read_to_string("input.txt").unwrap();
+fn a1(input: String) {
     let lines = input.lines();
-
     let mut score = 0;
     for line in lines {
         let mut tokens = line.split(" ");
@@ -35,4 +50,25 @@ fn main() {
         score += score_choice(me) + score_round(opp, me);
     }
     println!("{}", score);
+}
+
+fn a2(input: String) {
+    let lines = input.lines();
+    let mut score = 0;
+    for line in lines {
+        let mut tokens = line.split(" ");
+        let opp = tokens.next().unwrap();
+        let me = tokens.next().unwrap();
+        let my_choice = get_choice(opp, me);
+        score += score_choice(my_choice) + score_round(opp, my_choice);
+    }
+    println!("{}", score);
+}
+
+fn main() {
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    //a1(input);
+    a2(input);
+
+    
 }
