2a6daef7e01ce20bc6e6ee70a4e5ac9aca6ad4a7
Magnus Morton <magnusmorton@gmail.com>
2023-08-01 23:10:01 +0100

    cleaned up

--- 2022/day7/src/main.rs
+++ 2022/day7/src/main.rs
@@ -18,17 +18,16 @@ fn main() {
     let dir_reg =Regex::new(r"dir (.+)").unwrap();
     let file_reg = Regex::new(r"(\d+) (.+)").unwrap();
     for line in str.lines() {
+        let clone = pwd.clone();
+        let mut brw = clone.borrow_mut();   
+        let children = brw.children.as_mut().unwrap();
         if let Some(cap) = cd_reg.captures(line) {
             let dir = cap.get(1).unwrap().as_str();
             if dir == "/" {
                 pwd = root.clone()
             } else if dir == ".." {
-                let tmp = pwd.borrow().parent.as_ref().unwrap().clone();
-                pwd = tmp;
+                pwd = brw.parent.as_ref().unwrap().clone();
             } else {
-                let clone = pwd.clone();
-                let brw = clone.borrow();
-                let children = brw.children.as_ref().unwrap();
                 let child = children.get(&dir.to_string());
                 pwd = child.unwrap().clone();
             }
@@ -36,18 +35,12 @@ fn main() {
         } else if let Some(cap) = dir_reg.captures(line) {
             let name = cap.get(1).unwrap().as_str().to_string();
             let namec = name.clone();
-            let clone = pwd.clone();
-            let mut brw = clone.borrow_mut();
-            let children = brw.children.as_mut().unwrap();
             if !children.contains_key(&name) {
                 children.insert(name, Rc::new(RefCell::new(FileNode { name: namec, size: 0, children: Some(HashMap::new()), parent: Some(pwd.clone()) })));
             }
         } else if let Some(cap) = file_reg.captures(line) {
             let name = cap.get(2).unwrap().as_str().to_string();
             let namec = name.clone();
-            let clone = pwd.clone();
-            let mut brw = clone.borrow_mut();
-            let children = brw.children.as_mut().unwrap();
             if !children.contains_key(&name) {
                 children.insert(name, Rc::new(RefCell::new(FileNode { name:namec, size: str::parse(cap.get(1).unwrap().as_str()).unwrap(), children: None, parent: Some(pwd.clone()) })));
             }
