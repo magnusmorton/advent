2c99b323e774967d5d9b43328dae12d4f23876d3
Magnus Morton <magnusmorton@gmail.com>
2025-12-29 01:03:45 +0000

    5.1 done

---
+++ 2025/5a.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int main()
+{
+    std::vector<std::pair<size_t, size_t>> ranges;
+    std::vector<size_t> queries;
+    std::string line;
+    bool parse_ranges = true;
+    while (std::getline(std::cin, line)) {
+        if (line == "") {
+            parse_ranges = false;
+        } else if (parse_ranges) {
+            size_t i = line.find("-");
+            std::string start = line.substr(0, i);
+            std::string end = line.substr(i+1);
+            size_t start_i = std::stoul(start);
+            size_t end_i = std::stoul(end);
+            ranges.push_back(std::make_pair(start_i, end_i));
+        } else {
+            queries.push_back(std::stoul(line));
+        }
+    }
+    std::sort(ranges.begin(), ranges.end());
+    std::vector<std::pair<size_t,size_t>> merged;
+    merged.push_back(ranges[0]);
+
+    for (int i =1; i < ranges.size(); i ++) {
+        auto& [lastl, lastr] = merged.back();
+        auto& [newl, newr] = ranges[i];
+
+        if (newl <= lastr && newr > lastr) {
+            lastr = newr;
+        } else {
+            merged.push_back(ranges[i]);
+        }
+        
+    }
+
+    size_t fresh = 0;
+    for (auto&& query : queries) {
+        bool spoiled = true;
+        for (auto&& [l, r]: merged) {
+            if (query >= l && query <= r) {
+                spoiled = false;
+                break;
+            }
+        }
+        if (!spoiled) {
+            fresh++;
+        }
+    }
+    std::cout << "fresh: " << fresh << std::endl;
+}
