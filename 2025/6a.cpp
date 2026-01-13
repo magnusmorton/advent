3c18bebe38ad6c15984d26191aefa94cf2f94b73
Magnus Morton <magnusmorton@gmail.com>
2026-01-13 23:27:19 +0000

    6a stub

---
+++ 2025/6a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::vector<std::vector<std::string>> tokens;
+    std::vector<std::string> lines;
+    std::string line;
+    while (std::getline(std::cin, line)) {
+        
+    }
+}
